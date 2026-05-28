b2299b2d12bc6e51e22b56805508809da8c3a580
narendrakumar1125 <n.narendrakumar1125@gmail.com>
2026-05-28 18:17:17 +0530

    Delete out directory

--- out/_next/static/chunks/app/client-admin/courses/[id]/page-db4e683c80a848f9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7974],{2819:function(n,e,u){Promise.resolve().then(u.bind(u,2514))}},function(n){n.O(0,[4358,874,4139,8792,448,2514,2971,8069,1744],function(){return n(n.s=2819)}),_N_E=n.O()}]);
